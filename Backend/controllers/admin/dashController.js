47221c428bc9d51f939c5d66a38c0b21c670f57b
Yadhu Krishna <yadhu808krishna@gmail.com>
2025-12-18 23:20:17 +0530

    Query updated for filtering but result alteration pending

--- Backend/controllers/admin/dashController.js
+++ Backend/controllers/admin/dashController.js
@@ -1,13 +1,26 @@
 // controllers/dataController.js
-import { getAllCities, getAllDrivers, getAllRoutes } from "../../services/admin/dashboardService.js"
+import { getAllCities, getAllDrivers, getAllRoutes, getAllottedCities, getAllottedDrivers, getAllottedRoutes} from "../../services/admin/dashboardService.js"
 
 export const getAllData = async (req, res) => {
   try {
-    const [cities, drivers, routes] = await Promise.all([
+    // console.log("Request user", req.user);
+    const {id:userId,role:userRole}= req.user;
+    let cities,drivers,routes;
+    if(userRole=='superadmin'){
+     [cities, drivers, routes] = await Promise.all([
       getAllCities(),
       getAllDrivers(),
       getAllRoutes(),
     ]);
+  }else{
+     [cities, drivers, routes] = await Promise.all([
+      getAllottedCities(userId),
+      getAllottedDrivers(userId),
+      getAllottedRoutes(userId),
+    ]);
+  }
+  
+
 
     res.status(200).json({
       success: true,
--- Backend/services/admin/dashboardService.js
+++ Backend/services/admin/dashboardService.js
@@ -18,3 +18,42 @@ export const getAllRoutes = async () => {
   const result = await pool.query(`SELECT * FROM routes WHERE enabled = true ORDER BY name ASC`);
   return result.rows;
 };
+
+export const getAllottedCities=async(id)=>{
+  const result=await pool.query(
+    `SELECT c.id as id  ,c.job,c.city_code,acr.admin_id FROM city c
+    JOIN admin_city_ref acr ON c.id=acr.city_id where acr.admin_id=$1 and enabled=true 
+    `,[id]
+  );
+  return result.rows;
+}
+
+export const getAllottedDrivers= async(id)=>{
+  const result=await pool.query(
+        `SELECT 
+      d.id,
+      d.name,
+      c.job AS city
+    FROM drivers d
+    JOIN city c ON c.id = d.city_id
+    JOIN admin_city_ref acr ON acr.city_id = c.id
+    WHERE acr.admin_id = $1
+      AND d.enabled = true;
+    `,[id]);
+    return result.rows;
+}
+
+export const getAllottedRoutes=async(id)=>{
+ const result = await pool.query(`
+   SELECT 
+  r.id,
+  r.name,
+  r.enabled,
+  c.job AS city
+FROM routes r
+JOIN city c ON c.job = r.job
+JOIN admin_city_ref acr ON acr.city_id = c.id
+WHERE acr.admin_id = $1
+  AND r.enabled = true`,[id]);
+  return result.rows;
+}
